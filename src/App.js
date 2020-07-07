c28a14b8d79272dffefdbe560a53b57962309faa
rokasandreikenas <shyptt@gmail.com>
2020-07-07 17:03:57 +0300

    add components: cards, chart

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.scss";
+import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
+
+class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="container">
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CountryPicker.scss";
+
+const CountryPicker = () => {
+  return <div>CountryPicker</div>;
+};
+
+export default CountryPicker;
