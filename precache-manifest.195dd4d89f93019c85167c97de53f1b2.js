dc1baa9109c9da2f9948f9d6dfec66cec528c382
rokasandreikenas <shyptt@gmail.com>
2020-07-14 16:13:09 +0300

    Updates

---
+++ precache-manifest.195dd4d89f93019c85167c97de53f1b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d018ef01f70561de74078bef112cbc",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "dbca6cc495d2f62a3186",
+    "url": "/covid-tracker/static/css/main.2525272b.chunk.css"
+  },
+  {
+    "revision": "2adcca67e1779ca84959",
+    "url": "/covid-tracker/static/js/2.3abf807c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.3abf807c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbca6cc495d2f62a3186",
+    "url": "/covid-tracker/static/js/main.20095e4e.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covid.d7265326.png"
+  }
+]);
